aa6f7b80ac5098d4847e98c9833f5fb09369bae1
AbhishekChd <acechd97@gmail.com>
2017-01-07 19:18:37 +0530

    Added weather map api and google geolocation

--- Weather Application/script.js
+++ Weather Application/script.js
@@ -1,6 +1,8 @@
-var html ='',city = "",region = "",country = "",loc = "",lon = "",lat = "",url = "";
+var html ='',city = "",region = "",country = "",loc = "",lon = "",lat = "";
 var locationArr = [];
-
+var client_id = 'appid=0eba625878be18cb986e5462b3a6e92e';
+var url = 'http://api.openweathermap.org/data/2.5/weather';
+var weather_url;
 var convert = function(C) {
   var F;
   F = (C * (9 / 5)) + 32;
@@ -37,38 +39,65 @@ var findWeather = function() {
 }
 
 var getWeather = function(unit) {
-  var speedUnit = "m/s";
-  $.getJSON('http://ip-api.com/json', function(data) {
-    /*Fetching location data*/
-    /* location format is {
-                           "status": "success",
-                           "country": "United States",
-                           "countryCode": "US",
-                           "region": "CA",
-                           "regionName": "California",
-                           "city": "San Francisco",
-                           "zip": "94105",
-                           "lat": "37.7898",
-                           "lon": "-122.3942",
-                           "timezone": "America\/Los_Angeles",
-                           "isp": "Wikimedia Foundation",
-                           "org": "Wikimedia Foundation",
-                           "as": "AS14907 Wikimedia US network",
-                           "query": "208.80.152.201"
-                         } */
-    city = data.city;
-    regionName = data.regionName;
-    country = data.country;
-    var countryCode = data.countryCode;
-    loc = data.loc;
-    lat = data.lat;
-    lon = data.lon;
+    var speedUnit = "m/s";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            lat = position.coords.latitude
+            lon = position.coords.longitude;
+            weather_url = url+'?lat='+lat+'&lon='+lon+'&'+client_id+'&units=metric';
+                $.getJSON(weather_url, function(data) {
+      
+    
+      var pressure = Math.round((data['main']['pressure']) / 10);
+      var myDateM = new Date(data['sys']['sunrise'] * 1000);
+      myDateM = myDateM.toLocaleString()
+      var arr = myDateM.split(',');
+      var date = arr[0];
+      myDateM = arr[1];  
+      var myDateN = new Date(data['sys']['sunset'] * 1000);
+      myDateN = myDateN.toLocaleString()
+      var arr = myDateN.split(',');
+      myDateN = arr[1];
+      var w = data['weather'][0]['id'];
+      if ((w>=701 && w<=781)||(w>=801 && w<=804)) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clouds-background.jpg')";
+      } else if (w == 800) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clear-background.jpg')";
+      } else if ((w>=200 && w<=232)||(w>=300 && w<=321)||(w>=500 && w<=531)) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-rainy-background.jpg')";
+      } else if (w>=600 && w<=622) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-snow-background.jpg')";
+      } else {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-default-background.jpg')";
+      }
+      var t = data['main']['temp'],
+        t_min = data['main']['temp_min'],
+        t_max = data['main']['temp_max'];
+      if (unit == 'F') {
+        t = convert(t);
+        t_min = convert(t_min);
+        t_max = convert(t_max);
+      }
 
-    url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=0eba625878be18cb986e5462b3a6e92e&type=accurate&mode=json&units=metric";
-    //Fetching location data=========================
-    //Fetching and printing Weather data
-    $.getJSON(url, function(json) {
-      /*
+      htmlCode = '<pre><b>Weather   </b>:' + '<b>' + data['weather'][0]['main'] + '</b>, ' + data['weather'][0]['description'] + '<br/><b>Location  </b>:' + city + ' ' + data['name'] + ',<b> ' + country + '(' + data['sys']['country'] + ')' + '</b>' + '<br/><b>Humidity  </b>:' + data['main']['humidity'] + '%' + '<br/><b>Pressure  </b>:' + pressure + ' kPa' + '<br/><b>Date      </b>:' + date + '<br/><b>Sunrise   </b>:' + myDateM + '<br/><b>Sunset    </b>:' + myDateN + '<br/><b>Latitude  </b>:' + lat + '<br/><b>Longitude </b>:' + lon + '<br/><b>Wind Speed</b>:' + data['wind']['speed'] + ' m/s</pre>';
+
+      document.getElementById('weather').innerHTML = data['weather'][0]['main'];
+      document.getElementById('temp-main').innerHTML = 'Temp: ' + t;
+      document.getElementById('temp-min').innerHTML = 'Min: ' + t_min;
+      document.getElementById('temp-max').innerHTML = 'Max: ' + t_max;
+      document.getElementById('location').innerHTML = htmlCode;
+    });
+
+        });
+    }
+    else{
+        console.log("Can't access location");
+    }
+    
+    
+
+}
+/*
           Weather format
           {"coord" :{"lon":77.32,"lat":28.43},
           "weather":[{"id":501,"main":"Rain","description":"moderate rain","icon":"10n"}],
@@ -86,45 +115,3 @@ var getWeather = function(unit) {
           "cod":200
           }
           */
-      var pressure = Math.round((json['main']['pressure']) / 10);
-      var myDateM = new Date(json['sys']['sunrise'] * 1000);
-      myDateM = myDateM.toLocaleString()
-      var arr = myDateM.split(',');
-      var date = arr[0];
-      myDateM = arr[1];  
-      var myDateN = new Date(json['sys']['sunset'] * 1000);
-      myDateN = myDateN.toLocaleString()
-      var arr = myDateN.split(',');
-      myDateN = arr[1];
-      var w = json['weather'][0]['main'];
-      if (w == 'Clouds') {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clouds-background.jpg')";
-      } else if (w == 'Clear') {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clear-background.jpg')";
-      } else if (w == 'Thunderstorm' || w == 'Drizzle' || w == 'Rain') {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-rainy-background.jpg')";
-      } else if (w == 'Snow') {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-snow-background.jpg')";
-      } else {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-default-background.jpg')";
-      }
-      var t = json['main']['temp'],
-        t_min = json['main']['temp_min'],
-        t_max = json['main']['temp_max'];
-      if (unit == 'F') {
-        t = convert(t);
-        t_min = convert(t_min);
-        t_max = convert(t_max);
-      }
-
-      htmlCode = '<pre><b>Weather   </b>:' + '<b>' + json['weather'][0]['main'] + '</b>, ' + json['weather'][0]['description'] + '<br/><b>Location  </b>:' + city + ', ' + regionName + ',<b> ' + country + '(' + countryCode + ')' + '</b>' + '<br/><b>Humidity  </b>:' + json['main']['humidity'] + '%' + '<br/><b>Pressure  </b>:' + pressure + ' kPa' + '<br/><b>Date      </b>:' + date + '<br/><b>Sunrise   </b>:' + myDateM + '<br/><b>Sunset    </b>:' + myDateN + '<br/><b>Latitude  </b>:' + lat + '<br/><b>Longitude </b>:' + lon + '<br/><b>Wind Speed</b>:' + json['wind']['speed'] + ' m/s</pre>';
-
-      document.getElementById('weather').innerHTML = json['weather'][0]['main'];
-      document.getElementById('temp-main').innerHTML = 'Temp: ' + t;
-      document.getElementById('temp-min').innerHTML = 'Min: ' + t_min;
-      document.getElementById('temp-max').innerHTML = 'Max: ' + t_max;
-      document.getElementById('location').innerHTML = htmlCode;
-
-    });
-  });
-}
