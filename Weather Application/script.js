73bda2d70e1869233d6b480ca36434d4d84f3ad0
Abhishek Chaudhary <acechd97@gmail.com>
2019-01-19 16:31:33 +0530

    added geolocation support

--- Weather Application/script.js
+++ Weather Application/script.js
@@ -1,96 +1,107 @@
-var html ='',city = "",region = "",country = "",loc = "",lon = "",lat = "";
-var locationArr = [];
-var client_id = 'appid=0eba625878be18cb986e5462b3a6e92e';
-var url = 'http://api.openweathermap.org/data/2.5/weather';
-var weather_url;
-var convert = function(C) {
+var WEATHER_API_URL = 'https://fcc-weather-api.glitch.me/api/current';
+
+var html = "",
+  city = "",
+  region = "",
+  country = "",
+  loc = "",
+  lon = "",
+  lat = "";
+
+var convert = function (C) {
   var F;
   F = (C * (9 / 5)) + 32;
   return F.toFixed(2);
-}
-var toFahrenheit = function() {
-  getWeather('F');
+};
+var toFahrenheit = function () {
+  getLocation;
   var y = document.getElementsByClassName("unit");
   var i;
   for (i = 0; i < y.length; i++) {
-    y[i].innerHTML = '(&#8457;)';
+    y[i].innerHTML = '&#8457;';
   }
-  $('#ButtonC').removeClass('btn-primary')
-  $('#ButtonF').addClass('btn-primary')
-  $('#ButtonC').addClass('btn-default')
-}
-var toCelsius = function() {
-  getWeather('C');
+  $('#ButtonC').removeClass('btn-primary');
+  $('#ButtonF').addClass('btn-primary');
+  $('#ButtonC').addClass('btn-default');
+};
+var toCelsius = function () {
+  getLocation;
   var y = document.getElementsByClassName("unit");
   var i;
   for (i = 0; i < y.length; i++) {
-    y[i].innerHTML = '(&#8451;)';
+    y[i].innerHTML = '&#8451;';
   }
-  $('#ButtonF').removeClass('btn-primary')
-  $('#ButtonC').addClass('btn-primary')
-  $('#ButtonF').addClass('btn-default')
-}
-var findWeather = function() {
+  $('#ButtonF').removeClass('btn-primary');
+  $('#ButtonC').addClass('btn-primary');
+  $('#ButtonF').addClass('btn-default');
+};
+var findWeather = function () {
   if ($('#ButtonC').hasClass('btn-primary')) {
     toCelsius();
   } else if ($('#ButtonF').hasClass('btn-primary')) {
     toFahrenheit();
   }
+};
+
+var getLocation = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getWeatherData);
+  }else{
+    console.log("Geo Location not supported");
+  }
+};
+
+var getWeatherData = function (locationData) {
+  var lat = locationData['coords']['latitude'],
+      lon = locationData['coords']['longitude'];
+  $.ajax({
+    url: WEATHER_API_URL,
+    data: {
+      lat: lat,
+      lon: lon
+    },
+    type: 'GET',
+    dataType: 'json',
+    success: renderData
+  });
 }
 
-var getWeather = function(unit) {
-    var speedUnit = "m/s";
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
-            lat = position.coords.latitude
-            lon = position.coords.longitude;
-            weather_url = url+'?lat='+lat+'&lon='+lon+'&'+client_id+'&units=metric';
-                $.getJSON(weather_url, function(data) {
-      
-    
-      var pressure = Math.round((data['main']['pressure']) / 10);
-      var myDateM = new Date(data['sys']['sunrise'] * 1000);
-      myDateM = myDateM.toLocaleString()
-      var arr = myDateM.split(',');
-      var date = arr[0];
-      myDateM = arr[1];  
-      var myDateN = new Date(data['sys']['sunset'] * 1000);
-      myDateN = myDateN.toLocaleString()
-      var arr = myDateN.split(',');
-      myDateN = arr[1];
-      var w = data['weather'][0]['id'];
-      if ((w>=701 && w<=781)||(w>=801 && w<=804)) {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clouds-background.jpg')";
-      } else if (w == 800) {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-clear-background.jpg')";
-      } else if ((w>=200 && w<=232)||(w>=300 && w<=321)||(w>=500 && w<=531)) {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-rainy-background.jpg')";
-      } else if (w>=600 && w<=622) {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-snow-background.jpg')";
-      } else {
-        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/AbhishekChd/FreeCodeCampProjects/master/images/Weather-app-default-background.jpg')";
-      }
-      var t = data['main']['temp'],
-        t_min = data['main']['temp_min'],
-        t_max = data['main']['temp_max'];
-      if (unit == 'F') {
-        t = convert(t);
-        t_min = convert(t_min);
-        t_max = convert(t_max);
-      }
+var renderData = function (data) {
+  console.log(data)
+  var temp = data['main']['temp'],
+    weather_type = data['weather'][0]['main'],
+    weather_desc = data['weather'][0]['description'],
+    icon = data['weather'][0]['icon'],
+    iso_date = new Date(data['dt'] * 1000),
+    date = iso_date.toLocaleDateString(),
+    time_updated = iso_date.toLocaleTimeString(),
+    pressure = data['main']['pressure'],
+    humidity = data['main']['humidity'],
+    sunrise = new Date(data['sys']['sunrise'] * 1000).toLocaleTimeString(),
+    sunset = new Date(data['sys']['sunset'] * 1000).toLocaleTimeString(),
+    lat = data['coord']['lat'],
+    lon = data['coord']['lon'],
+    windSpeed = data['wind']['speed'],
+    inclination = data['wind']['deg'];
 
-      htmlCode = '<pre><b>Weather   </b>:' + '<b>' + data['weather'][0]['main'] + '</b>, ' + data['weather'][0]['description'] + '<br/><b>Location  </b>:' + city + ' ' + data['name'] + ',<b> ' + country + '(' + data['sys']['country'] + ')' + '</b>' + '<br/><b>Humidity  </b>:' + data['main']['humidity'] + '%' + '<br/><b>Pressure  </b>:' + pressure + ' kPa' + '<br/><b>Date      </b>:' + date + '<br/><b>Sunrise   </b>:' + myDateM + '<br/><b>Sunset    </b>:' + myDateN + '<br/><b>Latitude  </b>:' + lat + '<br/><b>Longitude </b>:' + lon + '<br/><b>Wind Speed</b>:' + data['wind']['speed'] + ' m/s</pre>';
-
-      document.getElementById('weather').innerHTML = data['weather'][0]['main'];
-      document.getElementById('temp-main').innerHTML = 'Temp: ' + t;
-      document.getElementById('temp-min').innerHTML = 'Min: ' + t_min;
-      document.getElementById('temp-max').innerHTML = 'Max: ' + t_max;
-      document.getElementById('location').innerHTML = htmlCode;
-    });
-
-        });
-    }
-    else{
-        console.log("Can't access location");
-    }
+  html = '<pre><b>Weather </b>:<b>' + weather_type + '</b>, ' + weather_desc +
+    '<br><b>Location  </b>:<b>' + city + ' ' + data['name'] + ',<b> ' + country + '(' + data['sys']['country'] + ')' + '</b>' +
+    '<br/><b>Humidity  </b>:' + humidity + '%' +
+    '<br/><b>Pressure </b>: ' + pressure + ' kPa' +
+    '<br/><b>Date Updated </b>: ' + date +
+    '<br/><b>Sunrise </b>: ' + sunrise +
+    '<br/><b>Sunset </b>: ' + sunset +
+    '<br/><b>Latitude  </b>:' + lat +
+    '<br/><b>Longitude </b>:' + lon +
+    '<br/><b>Wind Speed</b>:' + windSpeed + ' m/s at ' + inclination + 'deg</pre>';
+  $('#location').html(html);
+  $('#temp-main').html(temp + ' (&#8451;)');
+  $('#weather').text(weather_type);
+  $('#icon').find("img").attr({
+    src: icon
+  });
+  console.log(html);
 }
+$(function () {
+  $(getLocation);
+});
